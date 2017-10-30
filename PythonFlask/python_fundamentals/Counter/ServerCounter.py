44aeca8e214efea7878c1b30463dc428a8cb27cc
ccadenc <ccadenc@amazon.com>
2017-10-30 14:39:24 -0700

    counter made

--- PythonFlask/python_fundamentals/Counter/ServerCounter.py
+++ PythonFlask/python_fundamentals/Counter/ServerCounter.py
@@ -6,11 +6,18 @@ app.secret_key = 'ThisisSecret'
 @app.route('/')
 def index():
     try:
-        session['counter']
+        session['counter'] += 1
     except:
         session['counter'] = 0
 
-    return render_template('index.html', session['counter'])
+    return render_template('result.html', counter=session['counter'])
+
+
+@app.route('/info', methods=["POST"])
+def info():
+    session['counter'] += 1
+    print session['counter']
+    return redirect('result.html')
 
 
 app.run(debug=True)
