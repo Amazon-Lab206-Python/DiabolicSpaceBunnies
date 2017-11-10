4c22a6f7669059f98784afc6c5421afd95050b0c
ccadenc <ccadenc@amazon.com>
2017-11-10 13:34:14 -0700

    users models

---
+++ pythonDjango/users/apps/users_app/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, HttpResponse, redirect
+
+
+def index(request):
+    response: "Hi, I'm a bunny, and this is a request!"
+    return HttpResponse(request)
